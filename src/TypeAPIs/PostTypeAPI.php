8a0a36f8f3b7cf4e00be156956a2056cfc755c29
Leo <losoviz@gmail.com>
2019-12-20 19:21:43 +0800

    Pass $postObjectOrID to `getPermalink`

--- src/TypeAPIs/PostTypeAPI.php
+++ src/TypeAPIs/PostTypeAPI.php
@@ -9,15 +9,17 @@ use PoP\Translation\Facades\TranslationAPIFacade;
  */
 class PostTypeAPI extends \PoP\PostsWP\TypeAPIs\PostTypeAPI
 {
-    public function getExcerpt($post_id): ?string
+    public function getExcerpt($postObjectOrID): ?string
     {
-        $post = $this->getPost($post_id);
-	    $readmore = sprintf(
+        list(
+            $post,
+            $postID,
+        ) = $this->getPostObjectAndID($postObjectOrID);
+        $readmore = sprintf(
 	        TranslationAPIFacade::getInstance()->__('... <a href="%s">Read more</a>', 'pop-application'),
-	        $this->getPermalink($post_id)
+	        $this->getPermalink($postObjectOrID)
 	    );
 	    $value = empty($post->post_excerpt) ? \limitString(\strip_tags(\strip_shortcodes($post->post_content)), $this->getExcerptLength(), $readmore) : $post->post_excerpt;
-
-	    return HooksAPIFacade::getInstance()->applyFilters('get_the_excerpt', $value, $post_id);
+	    return HooksAPIFacade::getInstance()->applyFilters('get_the_excerpt', $value, $postID);
     }
 }

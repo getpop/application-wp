f826d3e375713c5a7e92d833edc39fc4b5f542a8
Leonardo Losoviz <leo@getpop.org>
2020-06-17 11:06:02 +0800

    Renamed vars

--- src/Conditional/Posts/TypeAPIs/PostTypeAPI.php
+++ src/Conditional/Posts/TypeAPIs/PostTypeAPI.php
@@ -12,17 +12,25 @@ use PoP\Translation\Facades\TranslationAPIFacade;
  */
 class CustomPostTypeAPI extends \PoP\CustomPostsWP\TypeAPIs\CustomPostTypeAPI
 {
-    public function getExcerpt($postObjectOrID): ?string
+    public function getExcerpt($customPostObjectOrID): ?string
     {
         list(
-            $post,
-            $postID,
-        ) = $this->getCustomPostObjectAndID($postObjectOrID);
+            $customPost,
+            $customPostID,
+        ) = $this->getCustomPostObjectAndID($customPostObjectOrID);
         $readmore = sprintf(
             TranslationAPIFacade::getInstance()->__('... <a href="%s">Read more</a>', 'pop-application'),
-            $this->getPermalink($postObjectOrID)
+            $this->getPermalink($customPostObjectOrID)
         );
-        $value = empty($post->post_excerpt) ? \limitString(\strip_tags(\strip_shortcodes($post->post_content)), $this->getExcerptLength(), $readmore) : $post->post_excerpt;
-        return HooksAPIFacade::getInstance()->applyFilters('get_the_excerpt', $value, $postID);
+        $value = empty($customPost->post_excerpt) ?
+            \limitString(
+                \strip_tags(
+                    \strip_shortcodes($customPost->post_content)
+                ),
+                $this->getExcerptLength(),
+                $readmore
+            ) :
+            $customPost->post_excerpt;
+        return HooksAPIFacade::getInstance()->applyFilters('get_the_excerpt', $value, $customPostID);
     }
 }
